3d6dbf6fbb16a9d7a9a6f50da832f99753bd13a7
pulgamecanica <pulgamecanica11@gmail.com>
2026-01-06 23:52:42 +0100

    chore: fix norm parsing file format detection

--- src/ft_nm.c
+++ src/ft_nm.c
@@ -1,38 +1,37 @@
 #include "ft_nm.h"
 #include "ft_nm_node.h"
 
-static void print_error(t_nm_node *node)
+static void	print_error(t_nm_node *node)
 {
-  ft_putstr_fd("ft_nm: '", 2);
-  ft_putstr_fd(node->filename, 2);
-  ft_putstr_fd("': ", 2);
-  if (node->error == NM_ERR_NO_FILE)
-    ft_putstr_fd("No such file\n", 2);
-  else if (node->error == NM_ERR_PERMISSION)
-    ft_putstr_fd("Permission denied\n", 2);
-  else if (node->error == NM_ERR_FSTAT)
-    ft_putstr_fd("fstat failed\n", 2);
-  else if (node->error == NM_ERR_MMAP)
-    ft_putstr_fd("mmap failed\n", 2);
-  else if (node->error == NM_ERR_NOT_ELF)
-    ft_putstr_fd("file format not recognized\n", 2);
+	ft_putstr_fd("ft_nm: '", 2);
+	ft_putstr_fd(node->filename, 2);
+	ft_putstr_fd("': ", 2);
+	if (node->error == NM_ERR_NO_FILE)
+		ft_putstr_fd("No such file\n", 2);
+	else if (node->error == NM_ERR_PERMISSION)
+		ft_putstr_fd("Permission denied\n", 2);
+	else if (node->error == NM_ERR_FSTAT)
+		ft_putstr_fd("fstat failed\n", 2);
+	else if (node->error == NM_ERR_MMAP)
+		ft_putstr_fd("mmap failed\n", 2);
+	else if (node->error == NM_ERR_NOT_ELF)
+		ft_putstr_fd("file format not recognized\n", 2);
 }
 
-static void process_node(t_nm_node *node, t_nm_options *opts)
+static void	process_node(t_nm_node *node, t_nm_options *opts)
 {
-  if (!node)
-    return ;
-  if (node->error != NM_OK)
-  {
-    print_error(node);
-    return ;
-  }
-  (void)opts;
-  printf("Processing file: %s\n", node->filename);
-  // TODO: Add symbol processing logic here
+	if (!node)
+		return ;
+	if (node->error != NM_OK)
+	{
+		print_error(node);
+		return ;
+	}
+	(void)opts;
+	printf("Processing file: %s\n", node->filename);
 }
 
-void nm(t_nm_options *opts)
+void	nm(t_nm_options *opts)
 {
-  ft_lstiter_param(opts->files, (void (*)(void *, void *))process_node, opts);
+	ft_lstiter_param(opts->files, (void (*)(void *, void *))process_node, opts);
 }
